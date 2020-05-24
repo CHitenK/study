5842f3a862074ed48b423bfc8fed010d732f0868
CHitenK <1216430032@qq.com>
2020-05-24 21:26:04 +0800

    feat 提交node生成图片的代码

---
+++ node-img/serve.js
@@ -0,0 +1,79 @@
+const Koa = require('koa')
+const app = new Koa()
+
+const sharp = require('sharp')
+const textToSVG = require('text-to-svg').loadSync()
+const Router = require('koa-router')
+const fs = require('fs')
+const path = require('path')
+const mime = require('mime-types') //需npm安装
+const router = new Router()
+var http = require('http')
+var url = require('url')
+
+router.get('/image', async (ctx, next) => {
+  // ctx.response.body = '<h5>Index</h5>';
+  try {
+    let options = {
+      x: 0,         //文本开头的水平位置（默认值：0）
+      y: 0,         // 文本的基线的垂直位置（默认值：0）
+      fontSize: 18, // 字体大小
+      anchor: 'top', // 坐标中的对象锚点
+      attributes: {
+          fill: '#000' // 文字颜色
+      }
+    }
+    let textSVG = textToSVG.getSVG('我的店铺名', options)
+    const img = await getImgs('http://chimke.cn/images/add.png')
+    console.log(textSVG)
+    const svgBuffer = Buffer.from(textSVG)
+    const semiTransparentRedPng1 = await sharp({
+      create: {
+        width: 400,
+        height: 400,
+        channels: 4,
+        background: { r: 255, g: 255, b: 255, alpha: 1 }
+      }
+    }).composite([{
+      input: semiTransparentRedPng,
+      gravity: 'southeast',
+      top: 30,
+      left: 15
+    }]).png()
+
+    console.log(semiTransparentRedPng)
+
+    ctx.set('content-type', 'image/png')
+    ctx.response.body = semiTransparentRedPng1
+  } catch(err) {
+    console.log('失败',err)
+    ctx.response.body = {
+      code: 500,
+      data: {},
+      masg: '出错了'
+    }
+  }
+  
+})
+
+
+app.use(router.routes())
+app.listen(2020, function() {
+  console.log('----------------------2020端口启动了-------------------------')
+})
+
+function getImgs(imgurl) {
+  var options = url.parse(imgurl)
+  return new Promise((resolve, reject) => {
+    // 请求图片资源
+    http.get(options, function (response) {
+      var chunks = []
+      response.on('data', function (chunk) {
+        chunks.push(chunk)
+      }).on('end', function() {
+        var buffer = Buffer.concat(chunks)
+        resolve(buffer)
+      });
+    })
+  })
+}
---
+++ node-img/serve_canvas.js
@@ -0,0 +1,55 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const router = new Router()
+const app = new Koa()
+var http = require('http')
+var https = require('https')
+var url = require('url')
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(400, 600, 'jpg')
+const ctx = canvas.getContext('2d')
+const path = require('path')
+
+router.get('/image', async (res, next) => {
+  ctx.fillStyle = '#ffffff'
+  ctx.fillRect(0, 0, 400, 600)
+  ctx.save()
+  ctx.font = '16px "Microsoft YaHei"'
+  ctx.fillStyle = '#000'
+  // ctx.rotate(0.1)
+  ctx.fillText('非常好!', 10, 20)
+  
+  // Draw line under text
+  var text = ctx.measureText('非常好!')
+  ctx.strokeStyle = '#000'
+  ctx.beginPath()
+  ctx.lineTo(50, 102)
+  ctx.lineTo(50 + text.width, 102)
+  ctx.stroke() 
+  ctx.save()
+  const myimg = await loadImage('http://chimke.cn/images/add.png')
+  ctx.drawImage(myimg, 10, 30, 60,60)
+  console.log(myimg)
+  res.set('content-type', 'image/png')
+  res.response.body = canvas.toBuffer()
+})
+
+app.use(router.routes())
+app.listen(2021, function() {
+  console.log('----------------------2021端口启动了-------------------------')
+})
+function getImgs(imgurl) {
+  var options = url.parse(imgurl)
+  return new Promise((resolve, reject) => {
+    // 请求图片资源
+    http.get(options, function (response) {
+      var chunks = []
+      response.on('data', function (chunk) {
+        chunks.push(chunk)
+      }).on('end', function() {
+        var buffer = Buffer.concat(chunks)
+        resolve(chunks)
+      });
+    })
+  })
+}
