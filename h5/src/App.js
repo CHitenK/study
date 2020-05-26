3588c6b27ce630261d048728dedabe560cdac1d8
CHitenK <1216430032@qq.com>
2020-05-26 19:13:03 +0800

    fixed 修复排序问题

---
+++ h5/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Button} from 'antd-mobile'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          
+        </a>
+      </header> */}
+      <Button  type="primary">按钮</Button>
+    </div>
+  );
+}
+
+export default App;
