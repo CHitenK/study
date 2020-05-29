493e20e00b1dc2ad4ef3d37641de8869581da52e
CHitenK <1216430032@qq.com>
2020-05-30 03:06:16 +0800

    测试

---
+++ h5/build/precache-manifest.45931bdb26cc4f70d1188fdd5a9eb851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6e0f88bcb1b50097ea",
+    "url": "/./static/js/2.8a66e93f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/./static/js/2.8a66e93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3fd799786ed923e85d8",
+    "url": "/./static/js/main.af96f593.chunk.js"
+  },
+  {
+    "revision": "8eaaeba5c15c2bd56d9d",
+    "url": "/./static/js/runtime-main.29c5cdea.js"
+  },
+  {
+    "revision": "38df4af169fcabc489a6aeae18429871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6e0f88bcb1b50097ea",
+    "url": "/static/css/2.cea97ed1.chunk.css"
+  },
+  {
+    "revision": "a3fd799786ed923e85d8",
+    "url": "/static/css/main.3ad8f0c3.chunk.css"
+  }
+]);
