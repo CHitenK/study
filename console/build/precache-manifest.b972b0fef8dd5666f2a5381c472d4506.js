764e7dfd6809859b50ea33bfb0438d41c237e498
CHitenK <1216430032@qq.com>
2020-05-31 17:59:19 +0800

    init

---
+++ console/build/precache-manifest.b972b0fef8dd5666f2a5381c472d4506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7b8531b2d422944efb9504a2ae7f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "086f669406b631aef24c",
+    "url": "/static/css/main.9f910234.chunk.css"
+  },
+  {
+    "revision": "7f45ddec230f8520eb3a",
+    "url": "/static/js/2.f01a5d7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f01a5d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086f669406b631aef24c",
+    "url": "/static/js/main.6922aad4.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
