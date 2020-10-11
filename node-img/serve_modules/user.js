8445fc5e1cd59d4cb54786c68fc560c1c49073ee
project <1216430032@qq.com>
2020-10-11 21:06:29 +0800

    fixed init

--- node-img/serve_modules/user.js
+++ node-img/serve_modules/user.js
@@ -1,4 +1,5 @@
 const Axios = require('axios')
+const sha1 = require('sha1')
 const Router = require('koa-router')
 const router = new Router()
 const User = require('./../db/user_db')
@@ -8,9 +9,20 @@ router.get('/user/info', async (ctx, next) => {
 })
 
 router.get('/user/token', async (ctx, next) => {
-  const data = await getWxToken()
-  console.log(data)
-  ctx.response.body = data
+  const data1 = await getWxToken()
+  const data2 = await getTac(data1)
+  const noncestr = 'wrerewOqpekichLKeu'
+  const timestamp = Date.now()
+  const url = 'http://chimke.cn:8083/pages/login/index'
+  const str = `jsapi_ticket=${data2}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`
+  const sh = sha1(url)
+  const yaya = {
+    appId: 'wx17d304d815b5193f', // 必填，公众号的唯一标识
+    timestamp: timestamp, // 必填，生成签名的时间戳
+    nonceStr: noncestr, // 必填，生成签名的随机串
+    signature: sh
+  }
+  ctx.response.body = yaya
 })
 function inset() {
     const user = new User({
@@ -39,8 +51,30 @@ function getWxToken() {
             method: 'GET',
             url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx17d304d815b5193f&secret=3f79fd1aa7db75aeb7c0ee51e7d23760'
         }).then(res => {
-            resoved(res.data)
+            console.log('access_token', res.data)
+            if (res.data.access_token) {
+                resoved(res.data.access_token)
+            } else {
+                reject(null)
+            }
+        }).catch(err => {
+            reject(null)
         })
     })
+}
+function getTac(data) {
+    return new Promise((resoved, reject) => {
+        Axios({
+            method: 'GET',
+            url: `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${data}&type=jsapi`
+        }).then(res => {
+            console.log('ticket', res.data)
+            if (res.data.ticket) {
+                resoved(res.data)
+            } else {
+                reject(null)
+            }
+        })
+    })    
 }
 module.exports = router
