10c6a6aae9d37ac2e5743e5b9940ab9045892c62
project <1216430032@qq.com>
2020-10-19 20:50:17 +0800

    fixed 积木

--- node-img/serve.js
+++ node-img/serve.js
@@ -13,56 +13,15 @@ const userRoter = require('./serve_modules/user')
 const makeImgRoter = require('./serve_modules/makeimg')
 const uploadRoute = require('./serve_modules/upload')
 const productRoute = require('./serve_modules/product')
-router.get('/image', async (ctx, next) => {
-  // ctx.response.body = '<h5>Index</h5>';
-  try {
-    let options = {
-      x: 0,         //文本开头的水平位置（默认值：0）
-      y: 0,         // 文本的基线的垂直位置（默认值：0）
-      fontSize: 18, // 字体大小
-      anchor: 'top', // 坐标中的对象锚点
-      attributes: {
-          fill: '#000' // 文字颜色
-      }
-    }
-    let textSVG = textToSVG.getSVG('我的店铺名', options)
-    const img = await getImgs('http://chimke.cn/images/add.png')
-    console.log(textSVG)
-    const svgBuffer = Buffer.from(textSVG)
-    const semiTransparentRedPng1 = await sharp({
-      create: {
-        width: 400,
-        height: 400,
-        channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 1 }
-      }
-    }).composite([{
-      input: semiTransparentRedPng,
-      gravity: 'southeast',
-      top: 30,
-      left: 15
-    }]).png()
+const jmRoute = require('./serve_modules/jm')
 
-    console.log(semiTransparentRedPng)
-
-    ctx.set('content-type', 'image/png')
-    ctx.response.body = semiTransparentRedPng1
-  } catch(err) {
-    console.log('失败',err)
-    ctx.response.body = {
-      code: 500,
-      data: {},
-      masg: '出错了'
-    }
-  }
-  
-})
 app.use(bodyParser())
 app.use(router.routes()) 
 app.use(userRoter.routes())
 app.use(makeImgRoter.routes())
 app.use(uploadRoute.routes())
 app.use(productRoute.routes())
+app.use(jmRoute.routes())
 app.listen(2020, '0.0.0.0', function() {
   console.log('----------------------2020端口启动了-------------------------')
 })
