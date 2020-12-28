cd948e7c0be7218276ec0973946a3cf1424a09a4
project <1216430032@qq.com>
2020-12-28 09:48:56 +0800

    fixed

---
+++ h5/build/precache-manifest.c5d35941f17747cdc3fe9444a808d145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affac9d3d3fe847720878309a522d86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "031b69f0d8f0daf7268f",
+    "url": "/static/css/2.ce9da53a.chunk.css"
+  },
+  {
+    "revision": "aba22b3c51d54a5e10c6",
+    "url": "/static/css/main.4e86888f.chunk.css"
+  },
+  {
+    "revision": "031b69f0d8f0daf7268f",
+    "url": "/static/js/2.723b4993.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.723b4993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba22b3c51d54a5e10c6",
+    "url": "/static/js/main.5f85ea9f.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
