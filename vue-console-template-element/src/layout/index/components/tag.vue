c7dab678e7487706d57d974006a0d9278b72314c
Chi <1216430032@qq.com>
2021-01-13 08:43:59 +0800

    ‘fixed git commit -m‘fixed

--- vue-console-template-element/src/layout/index/components/tag.vue
+++ vue-console-template-element/src/layout/index/components/tag.vue
@@ -83,7 +83,7 @@ export default {
     }
   },
   mounted () {
-    this.baseWid = this.$refs.tagbx.offsetWidth
+    this.baseWid = this.$refs.tagbx.clientWidth + 200
   },
   methods: {
     linkTo (data) {
@@ -125,7 +125,8 @@ export default {
     setScrollLeft: debounce(function() {
       const dom = document.getElementById(this.activePath) // offsietWidth
       if (dom) {
-        if (dom.offsetLeft + 160 > this.baseWid) {
+        console.log(dom.offsetLeft, this.baseWid)
+        if (dom.offsetLeft + 20 >= this.baseWid) {
           this.$refs.tagbx.scrollLeft = this.$refs['all-tags'].clientWidth
         } else {
           this.$refs.tagbx.scrollLeft = 0
@@ -144,135 +145,6 @@ export default {
 <style lang="scss" scoped >
 $toobWid: 46px;
 .right-tag-box{
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-}
-.tool-bx{
-  height: 100%;
-  width: $toobWid;
-  background-color: #ffffff;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .15);
-  text-align: center;
-  font-size: 30px;
-  color: #666;
-  cursor: pointer;
-  position: relative;
-  .tool-bx-dian{
-    display: inline-block;
-  }
-  &:hover .tool-list{
-    display: block;
-  }
-}
-.item-bx{
-  width: calc(100% - #{$toobWid});
-  height: 100%;
-  overflow-x: scroll;
-  background-color: #ffffff;
-  box-shadow: 0px 1px 2px rgba(0, 0, 0, .15);
-  scrollbar-width: none; /* Firefox */
-  -ms-overflow-style: none; /* IE 10+ */
-  &::-webkit-scrollbar {
-    display: none; /* Chrome Safari */
-  }
-}
-.item-tag-bx{
-  height: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  cursor: pointer;
-}
-.item-tag{
-  padding: 5px 10px;
-  border: solid 1px #eaeaea;
-  height: 60%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 5px;
-  font-size: 12px;
-  color: #999;
-  i {
-    margin-left: 3px;
-    font-size: 14px;
-  }
-  &:hover{
-    color: #00BFFF;
-    border: solid 1px #00BFFF;
-  }
-}
-.item-tag-act{
-  color: #00BFFF;
-  border: solid 1px #00BFFF;
-  &::before{
-    display: inline-block;
-    content: '';
-    width: 6px;
-    height: 6px;
-    background-color: #00BFFF;
-    border-radius: 50%;
-    margin-right: 3px;
-  }
-}
-.tool-list{
-  display: none;
-  width: 80px;
-  background-color: #ffffff;
-  position: absolute;
-  top: 100%;
-  right: 5px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .15);
-}
-.tool-list-item{
-  width: 100%;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 12px;
-  border-top: solid 1px #eaeaea;
-}
-.tool-list-item-ot{
-  width: 100%;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 12px;
-  border-top: solid 1px #eaeaea;
-  position: relative;
-  &:hover .link-list{
-    display: block;
-  }
-}
-.b-n{
-  border: 0;
-}
-.link-list{
-  display: none;
-  width: 120px;
-  max-height: 50vh;
-  overflow-y: scroll;
-  background-color: #ffffff;
-  font-size: 12px;
-  position: absolute;
-  top: 0;
-  right: 80px;
-  scrollbar-width: none; /* Firefox */
-  -ms-overflow-style: none; /* IE 10+ */
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .15);
-  &::-webkit-scrollbar {
-    display: none; /* Chrome Safari */
-  }
-}
-.link-list-item{
-  display: block;
-  text-align: center;
-  height: 40px;
-  line-height: 40px;
   width: 70%;
   margin: 0 auto;
   overflow: hidden;
